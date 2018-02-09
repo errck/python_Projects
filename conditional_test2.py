8fd1240a238c9ac8a305b4f2e919b34648740cc3
errck <zamoraerick1@gmail.com>
2018-02-09 10:24:56 -0600

    r

---
+++ conditional_test2.py
@@ -0,0 +1,16 @@
+name="Jim"
+print(name=="Jim")
+print(name!="Jim")
+print("\b")
+#lower() function
+print(name.lower()=="jim")
+age=21
+print(age==21)
+print(age!=21)
+print(age>22)
+print(age<23)
+print(age>=19)
+print(age<=23)
+
+
+
